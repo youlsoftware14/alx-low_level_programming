a32bbda0293a7bd437c84da5421dc2ae4865d622
youlsoftware14 <yssoufsoulama140586@gmail.com>
2022-06-05 23:34:17 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- *  main - Entyr point
- *  Description: prints two digits combination
- *  Return: Always 0 (success)
+ *   main - Prints 3 combination of numbers
+ * 
+ * Return: Always (Success)
  */
 int main(void)
 {
-int c, i;
+int c, i, k;
+
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
 {
-if (c < i)
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)	
 {
 putchar(c);
 putchar(i);
+putchar(k);
+		
+if (c != '7')
+{
+putchar(',');
+putchar(' ');
+}
 }
 }
 }
