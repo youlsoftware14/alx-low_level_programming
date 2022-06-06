1faa80e4331ddf79eaabb5d01f75b416f8579f73
youlsoftware14 <yssoufsoulama140586@gmail.com>
2022-06-06 12:21:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,8 +13,9 @@ for (c = '0'; c <= '9'; c++)
 for (i = '0'; i <= '9'; i++)
 {
 for (k = '0'; k <= '9'; k++)
-{															if (c < i && i < k)
-{															putchar(c);
+{
+if (c < i && i < k)
+putchar(c);
 putchar(i);
 putchar(k);
 if (c != '7')
