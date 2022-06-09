e2d15e1923ae660bd56ac46c226b9130478b3950
youlsoftware14 <yssoufsoulama140586@gmail.com>
2022-06-08 17:50:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h";
 /**
  * main - prints putchar
  * Return: always 0
@@ -14,5 +14,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 Return (0);
 }
