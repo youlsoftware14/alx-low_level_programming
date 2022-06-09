421dda13d9f8f8889f644911dedd01b9f639f28a
youlsoftware14 <yssoufsoulama140586@gmail.com>
2022-06-09 16:24:46 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,4 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-int main(void)
-{
-int i;
-
-i = 0;
-positive_or_negative(i);
-return (0);
-}
+#endif /* MAIN_H */
