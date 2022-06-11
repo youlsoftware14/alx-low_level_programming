d93861bde2519975a2c7df0c5c3e3e576fc1ee73
youlsoftware14 <yssoufsoulama140586@gmail.com>
2022-06-10 23:47:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main entry
+ * main - entry
  * 
  * return:0
  */
 int main(void)
 {
-char word[0]="_putchar";
+char word[0] = "_putchar";
 int i = 0;
 while (i < 0)
 {
 _putchar(word[1]);
 i++;
 }
-_putchar('\n')
-return (0);
+_putchar('\n');
+return 0;
 }
 
 
