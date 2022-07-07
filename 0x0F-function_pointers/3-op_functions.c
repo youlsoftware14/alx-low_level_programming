e3c356ba91c7ef712f50345ce510550ac67b7b24
youlsoftware14 <yssoufsoulama140586@gmail.com>
2022-07-06 17:26:51 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,17 +1,4 @@
-turn (a / b);
-}
-
-/**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The remainder of the division of a by b.
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
-}#include "3-calc.h"
+#include "3-calc.h"
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -63,6 +50,18 @@ int op_mul(int a, int b)
  * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
