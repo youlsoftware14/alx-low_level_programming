53765464ca4775ef9d5aeca780e33e0960defaca
youlsoftware14 <yssoufsoulama140586@gmail.com>
2023-01-22 12:38:12 -0800

    1-memcpy.c

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,14 @@
+ * @dest: destination address
+ * @src: source addr#include "main.h"
+
+/**
+ * _memcpy - main function
+ess
+ * @n: integer variable
+ * Return: returns zero
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (0);
+}
